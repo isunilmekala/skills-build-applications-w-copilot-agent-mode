ffd98f1d3f163124519b71e45f19dca4602ec0ee
Sunil M <83385574+isunilmekala@users.noreply.github.com>
2025-04-17 16:00:29 +0000

    Fix database name in settings from 'monafit_db' to 'octofit_db'

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'monafit_db',
+        'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
     }
